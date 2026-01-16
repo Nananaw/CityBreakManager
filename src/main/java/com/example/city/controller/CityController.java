f0105439e63cf9fac608f8726864a3f0bd61e792
Nananaw <gamingnananaw@gmail.com>
2026-01-16 09:37:54 -0500

    update mappings

--- src/main/java/com/example/city/controller/CityController.java
+++ src/main/java/com/example/city/controller/CityController.java
@@ -3,6 +3,7 @@ package com.example.city.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,41 +22,41 @@ public class CityController {
 
     private CityService cityService;
 
-    @GetMapping
+    @GetMapping("/getAllCities")
     public List<City> getAllCities() {
         return cityService.findAllCities();
     }
 
-    @GetMapping
+    @GetMapping("/getAllTrips")
     public List<Trip> getAllTrips() {
         return cityService.findAllTrips();
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/getCity/{id}")
     public ResponseEntity<City> getCityById(@PathVariable int id) {
         return cityService.findCityById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/getTrip/{id}")
     public ResponseEntity<Trip> getTripById(@PathVariable int id) {
         return cityService.findTripById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
-    @PostMapping
+    @PostMapping("/addCity")
     public City createCity(@RequestBody City city) {
         return cityService.createCity(city);
     }
 
-    @PostMapping
+    @PostMapping("/addTrip")
     public Trip createTrip(@RequestBody Trip trip) {
         return cityService.createTrip(trip);
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/updateCity/{id}")
     public ResponseEntity<City> updateCity(@PathVariable int id, @RequestBody City updatedCity) {
         City city = cityService.updateCity(id, updatedCity);
         if (city == null) {
@@ -64,7 +65,7 @@ public class CityController {
         return ResponseEntity.ok(city);
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/updateTrip/{id}")
     public ResponseEntity<Trip> updateTrip(@PathVariable int id, @RequestBody Trip updatedTrip) {
         Trip trip = cityService.updateTrip(id, updatedTrip);
         if (trip == null) {
@@ -73,11 +74,13 @@ public class CityController {
         return ResponseEntity.ok(trip);
     }
 
+    @DeleteMapping("/deleteCity/{id}")
     public ResponseEntity<Void> deleteCity(@PathVariable int id) {
         cityService.deleteCityById(id);
         return ResponseEntity.noContent().build();
     }
 
+    @DeleteMapping("/deleteTrip/{id}")
     public ResponseEntity<Void> deleteTrip(@PathVariable int id) {
         cityService.deleteTripById(id);
         return ResponseEntity.noContent().build();
